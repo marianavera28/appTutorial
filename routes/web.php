0f531750b9b60cef71a15aad7e8c7bc86beda6fe
MarianaVera <mariana280790@gmail.com>
2017-09-20 11:06:22 -0400

    Commit para establecer las rutas de un proyecto

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Crear una ruta con variable por get opcional
+Route::get('articles/{nombre?}', function ($nombre = "No coloco nombre") {
+    echo "El nombre que has colocado es: ".$nombre;
+});
+
+//Ruta para conectar por get con un controller
+/*Route::get('articles', [
+	'as' => 'articles',
+	'uses' => 'UserController@index'
+]);*/
+
+
+//Ruta para usar grupos usando un prefijo
+Route::group(['prefix'=>'articles'], function(){
+
+	Route::get('view/{article?}', function($article = "Vacio"){
+	    echo $article;
+	});
+
+});
