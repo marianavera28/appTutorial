4cf067ee25798516ebce0d88dceaa73b48c129fa
MarianaVera <mariana280790@gmail.com>
2017-09-20 11:30:00 -0400

    Commit con la creacion de relacion entre routes y controller

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+
+class TestController extends Controller
+{
+    public function view ($id){
+
+    	$article = Article::find($id);
+
+    	//para mostrar las relaciones
+		$article->category;
+		$article->user;
+		$article->tags;
+
+    	//helper de laravel para mostrar datos
+    	dd($article);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,22 @@ Route::get('articles/{nombre?}', function ($nombre = "No coloco nombre") {
     echo "El nombre que has colocado es: ".$nombre;
 });
 
-//Ruta para conectar por get con un controller
-/*Route::get('articles', [
-	'as' => 'articles',
-	'uses' => 'UserController@index'
-]);*/
-
-
+/*
 //Ruta para usar grupos usando un prefijo
 Route::group(['prefix'=>'articles'], function(){
 
 	Route::get('view/{article?}', function($article = "Vacio"){
 	    echo $article;
 	});
+
+});*/
+
+//Ruta para conectar por get con un controller
+Route::group(['prefix'=>'articles'], function(){
+
+	Route::get('view/{id}', [
+		'uses' => 'TestController@view',
+		'as' => 'articlesView'
+	]);
 
 });
